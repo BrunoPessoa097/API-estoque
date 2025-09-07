cb5d1a87ee192fc4eb2f44a92e7b9405b61bec2c
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-07 10:17:05 +0000

    Melhoria nos códigos.

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,7 +1,5 @@
 import express, { Application, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
-import cors from 'cors';
-import helmet from 'helmet';
 
 // Import local
 import swaggerSpecs from '../config/swagger/swaggerJsDoc';
@@ -10,12 +8,6 @@ import swaggerSpecs from '../config/swagger/swaggerJsDoc';
 const app: Application = express();
 //configurações dos middlewares.
 app.use(express.json());
-app.use(helmet());
-app.use(cors({
-  origin: 'http://localhost',
-  methods: ['GET']
-}));
-
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
