51da33b8bd5a2dec9e045555bf05235ae716024a
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-13 07:41:17 +0000

    Criando o categoriaAdd.
    - Criando o categoriaAdd.
    - Adicionando comentários.

--- src/controllers/categoriaControllers.ts
+++ src/controllers/categoriaControllers.ts
@@ -1,13 +1,44 @@
 import {Request, Response } from 'express';
+// Imports locais.
+import Categoria, { CategoriaDocument } from '../interfaces/categoriaInterfaces';
+import categoriaMongo from '../schemas/mongoose/categoriaSchema';
 /**
  * @description Adicionar ao banco de dados.
+ * @author Bruno Pessoa
  */
-const categoriaAdd = (req: Request, res: Response) => {
+const categoriaAdd = async (req: Request<{}, {}, Categoria>, res: Response) => {
   try{
-    res.status(200).json({
-      message: 'Categoria Adicionar'
+    // Criando um objeto com as entradas dos usuários.
+    const categoriaNova: CategoriaDocument = new categoriaMongo({
+      ...req.body
     });
-  }catch(error){
+
+    // Verificando se existe à categoria.
+    const busca = await categoriaMongo.findOne({nome: req.body.nome});
+
+    // Caso não exista.
+    if(!busca){
+      // Cadastra a nova categoria.
+      await categoriaNova.save().then((categoriaNova)=>{
+        res.status(201).json({
+          message: "Cadastrado",
+          data: categoriaNova
+        });
+      }).catch((error)=>{
+        res.status(500).json({
+          error
+        })
+      });
+    }
+    // Retorna categoria existente.
+    else{
+      res.status(409).json({
+        message: 'A categoria já existe'
+      });
+    }
+  }
+  // Erro interno.
+  catch(error){
     res.status(500).json({
       message: 'Servidor Erro',
       error
