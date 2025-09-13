d99cef71a25424b4b7132bacf7fcd7efaf58b841
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-13 07:42:17 +0000

    Adicionando swagger na rota post Categoria
    - Adicionando o Swagger a rota de categoria.
    - Adicionando os comentários.

--- src/routes/categoriaRouter.ts
+++ src/routes/categoriaRouter.ts
@@ -5,6 +5,38 @@ import categoriaAdd from '../controllers/categoriaControllers'
 
 const categoriaRouter: Router = Router();
 
+/**
+ * @swagger
+ * tags: Categoria
+ * description: Categoria de produtos.
+ */
+
+/**
+* @swagger
+* /categoria:
+*  post:
+*    summary: Adicionar nova categoria
+*    description: Cria uma nova categoria.
+*    tags: [Categoria]
+*    requestBody:
+*      required: true
+*      content:
+*        application/json:
+*          schema:
+*            type: object
+*            properties:
+*              nome:
+*                type: string
+*              descricao:
+*                type: string
+*    responses:
+*      201:
+*        description: Categoria criada com sucesso
+*      409:
+*        description: Categoria já existe
+*      500:
+*        description: Erro no servidor
+*/
 categoriaRouter.route('/categoria')
   .post(categoriaValidar, categoriaPadronizar,categoriaAdd);
 
