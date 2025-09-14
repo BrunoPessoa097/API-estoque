cf092890b6ee52988b8c74af8302f00dd3095729
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-14 21:02:51 +0000

    Adicionando buscar por Id na categoria.
    - Adicionando buscar por id

--- src/controllers/categoriaControllers.ts
+++ src/controllers/categoriaControllers.ts
@@ -49,7 +49,7 @@ export const categoriaAdd = async (req: Request<{}, {}, Categoria>, res: Respons
  * @description Lista todas as categorias.
  * @author Bruno Pessoa
  */
-export const categoriaAll = async(req: Request, res: Response<{message:string, dados: CategoriaDocument[]} | {message?: string, error:any}>) => {
+export const categoriaAll = async(req: Request, res: Response<{message:string, dados: CategoriaDocument[] | null} | {message?: string, error:any}>) => {
   try{
     // Lista todas as categorias.
     const categoria: CategoriaDocument[] = await categoriaMongo.find();
@@ -58,13 +58,45 @@ export const categoriaAll = async(req: Request, res: Response<{message:string, d
     res.status(200).json({
       message: "Todas as categorias",
       dados: categoria
-    })
+    });
   }
   // Erro do servidor.
   catch(error){
     res.status(500).json({
       message: 'Error no servidor',
       error
-    })
+    });
+  }
+}
+
+/**
+ * @description Buscar uma única categoria.
+ * @author Bruno Pessoa
+ */
+export const categoriaUnico = async(req: Request<{id: string}>, res: Response<{dados: CategoriaDocument| null}|{message?: string, error?:any}>) => {
+  try {
+    // Receber o id.
+    const id:string = req.params.id; 
+
+    // Buscando categoria prlo id.
+    await categoriaMongo.findById(id)
+      .then((dados)=>{
+        // Retornando a categoria.
+        res.status(200).json({
+          dados
+        });
+      }).catch((error)=>{
+        // Caso categoria não exista.
+        res.status(400).json({
+          message: 'Nao existe o ID desejado.'
+        });
+      })
+  }
+  catch(error){
+    // Erro servidor.
+    res.status(500).json({
+      message: 'Server Error',
+      error
+    });
   }
 }
