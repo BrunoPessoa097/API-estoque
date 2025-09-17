3ab12285c0ce208268b9ca6102d577941881f00d
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-17 02:58:57 +0000

    Interface marca documento.
    - Adicionando interface documento.
    - Herança de marca input.

--- src/interfaces/marcaInterface.ts
+++ src/interfaces/marcaInterface.ts
@@ -1,6 +1,12 @@
+import { Document } from 'mongoose';
 // Interface marca para a entrada dos usuários.
 export default interface marcaInput {
   nome: string;
   nomeSocial: string;
   cnpj: string
+}
+
+export interface marcaDocument extends marcaInput, Document {
+  createdAt?: Date;
+  updatedAt?: Date
 }
