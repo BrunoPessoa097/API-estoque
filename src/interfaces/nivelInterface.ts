f9b5bdb6d84ff2fb2e67b0b0e8bb0e261849eb40
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-19 04:35:47 +0000

    Criando interface documento
    - adicionando propriedades de tempo.
    - exportando interface documento.

--- src/interfaces/nivelInterface.ts
+++ src/interfaces/nivelInterface.ts
@@ -1,6 +1,13 @@
+import { Document } from 'mongoose';
+
 interface nivelInput {
   sigla: string;
   descricao: string
+}
+
+export interface nivelDocument extends nivelInput, Document {
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 export default nivelInput;
