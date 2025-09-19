e1b90972670a6a67b22ec5299301a1770876c3bc
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-19 05:16:59 +0000

    Comentários no nível interface

--- src/interfaces/nivelInterface.ts
+++ src/interfaces/nivelInterface.ts
@@ -1,13 +1,16 @@
 import { Document } from 'mongoose';
 
+// entrada do usuario para nivel
 interface nivelInput {
   sigla: string;
-  descricao: string
+  descricao: string;
 }
 
+// nivel document
 export interface nivelDocument extends nivelInput, Document {
   createdAt?: Date;
   updatedAt?: Date;
 }
 
+// export
 export default nivelInput;
