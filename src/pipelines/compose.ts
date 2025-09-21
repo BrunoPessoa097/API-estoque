71015673a913e1c801ac490a6c28873e5737e531
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-21 09:56:33 +0000

    Adicionando comentários

--- src/pipelines/compose.ts
+++ src/pipelines/compose.ts
@@ -1,5 +1,10 @@
 import { RequestHandler } from 'express';
 
+/**
+ * @description função para middlewares
+ * @param {middlewares} - middlewares
+ * @return array
+ */
 export const compose = (...middlewares: RequestHandler[]):RequestHandler[] => {
   return middlewares;
 }
--- src/pipelines/nivelPipelines.ts
+++ src/pipelines/nivelPipelines.ts
@@ -1,17 +1,20 @@
 import {compose }from './compose';
 import {nivelVerificar, nivelPadronizar } from '../middlewares/nivelMiddlewares';
 import {nivelList, nivelAdd, nivelUpdate,nivelId,nivelDelete} from '../controllers/nivelControllers';
-  
+
+// listar os níveis 
 export const nivelPipList = compose(
   nivelList
 );
 
+// adicionar nivel
 export const nivelPipAdd = compose(
   nivelVerificar,
   nivelPadronizar,
   nivelAdd
 );
 
+// mostrar único nivel
 export const nivelPipId = compose(
   nivelId
 )
@@ -22,6 +25,7 @@ export const nivelPipUpdate = compose(
   nivelUpdate
 );
 
+// deletar nível 
 export const nivelPipDelete = compose(
   nivelDelete
 );
