a4e14b93822ad3a17b1d9f619a6589044d7a2990
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-22 22:32:27 +0000

    Validação das entradas de produtos.
    - criando validações com o joi para produtos.
    - adicionando para validar.
    - adição de comentários

--- src/middlewares/produtoMiddlewares.ts
+++ src/middlewares/produtoMiddlewares.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from 'express';
+import { ObjectSchema } from 'joi';
 // imports locais 
 import produtoInput from '../interfaces/produtoInterface';
+import produtoJoi from '../schemas/joi/produtoJoi';
 
+/**
+ * @description verificar se as entradas de produtos foram preenchidos corretamente
+ * @author Bruno Pessoa
+ */
 const produtoVerificar = (req: Request<{}, {}, produtoInput>, res: Response) => {
   try{
+    // destruturação das informações.
     const { nome, quantidade, preco, id_marca, id_categoria }: produtoInput = req.body;
 
+    // removendo espaços em branco.
     req.body = {
       nome: nome.trim(),
       quantidade,
@@ -14,12 +22,25 @@ const produtoVerificar = (req: Request<{}, {}, produtoInput>, res: Response) => 
       id_categoria: id_categoria.trim()
     }
 
-    
-    res.status(200).json({
-      message: 'ok',
-      dados: req.body
-    });
+    // verificando as entradas com joi
+    const { error, value } = produtoJoi.validate(req.body,{abortEarly: false});
+
+    // senão tiver error
+    if(!error){
+      req.body = value;
+      res.status(200).json({
+        message: req.body
+      });
+    }
+    // em caso de erro
+    else{
+      res.status(404).json({
+        message: "Dados com erros",
+        error: error.details.map(e=>e.message)
+      });
+    }
   }
+  // servidor error
   catch(error){
     res.status(500).json({
       message: 'Server error',
---
+++ src/schemas/joi/produtoJoi.ts
@@ -0,0 +1,38 @@
+import Joi,{ ObjectSchema } from 'joi';
+// import local
+import produtoInput from '../../interfaces/produtoInterface';
+
+// produto joi
+const produtoJoi: ObjectSchema<produtoInput> = Joi.object({
+  nome: Joi.string().min(3).max(50).required().messages({
+    "string.empty": "Nome do produto nao pode estar em branco",
+    "string.min": "Nome do produto tem que ter no mínimo {#limit} caracteres",
+    "string.max": "Nome do produto tem que ter no máximo {#limit} caracteres",
+    "any.required": "Campo nome requerido"
+  }),
+  quantidade: Joi.number().integer().positive().required().messages({
+    "number.base": "Quantidade tem que ser um número",
+    "number.positive": "Quantidade não pode ser um número negativo",
+    "any.required": "Quantidade é obrigatório"
+  }),
+  preco: Joi.number().positive().required().messages({
+    "number.base": "Preço deve ser um número",
+    "number.positive": "Preço tem que ser um valor positivo",
+    "any.required":"Campo preço não pode ser vazio"
+  }),
+  id_marca: Joi.string().length(24).required().messages({
+    "string base": "ID marca tem que ser um texto",
+    "string.empty": "ID da marca não pode estar vazio",
+    "string.length": "O ID marca tem que ter exatamente {#limit} caracteres",
+    "any.required": "O ID marca e obrigatório"
+  }),
+  id_categoria: Joi.string().length(24).required().messages({
+    "string.base": "O ID categoria tem que ser um texto",
+    "string.empty": "O ID cateria não pode estar vazio",
+    "string.length": "O ID categoria tem que ter exatamente {#limit} caracteres",
+    "any.required": "O ID categoria é obrigatório"
+  })
+});
+
+// export 
+export default produtoJoi;
