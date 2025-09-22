6342ab67130d410a7f0d5496bb836af039947589
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-22 20:08:53 +0000

    Removendo espepacos em branco de produtos
    Removendo espaços em branco

--- src/middlewares/produtoMiddlewares.ts
+++ src/middlewares/produtoMiddlewares.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express';
+// imports locais 
+import produtoInput from '../interfaces/produtoInterface';
 
-const produtoVerificar = (req: Request, res: Response) => {
+const produtoVerificar = (req: Request<{}, {}, produtoInput>, res: Response) => {
   try{
+    const { nome, quantidade, preco, id_marca, id_categoria }: produtoInput = req.body;
+
+    req.body = {
+      nome: nome.trim(),
+      quantidade,
+      preco,
+      id_marca: id_marca.trim(),
+      id_categoria: id_categoria.trim()
+    }
+
+    
     res.status(200).json({
-      message: 'ok'
+      message: 'ok',
+      dados: req.body
     });
   }
   catch(error){
