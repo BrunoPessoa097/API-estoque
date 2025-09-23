fd93d8d25cb1816bf6a998d36d454f424ee653e5
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-23 05:20:21 +0000

    Produto comentários
    - adicionando rota produto add ao swagger.

--- src/routes/produtoRouter.ts
+++ src/routes/produtoRouter.ts
@@ -3,6 +3,47 @@ import prodPipAdd from '../pipelines/produtoPipelines';
 
 const produtoRouter: Router = Router();
 
+/**
+ * @swagger
+ * tags: Produtos
+ * summary: Rotas de produtos
+ */
+
+/**
+ * @swagger
+ * /produtos:
+ *  post:
+ *    summary:  Rota para adicionar nivel
+ *    description: Criar Produto
+ *    tags: [Produtos]
+ *    requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *          type: object
+ *          properties:
+ *           nome:
+ *            type: string
+ *           quantidade:
+ *            type: number
+ *           preco:
+ *            type: number
+ *           id_marca:
+ *            type: string
+ *           id_categoria:
+ *            type: string
+ *    responses:
+ *      201:
+ *       description: Adicionado
+ *      404:
+ *       description: Erro ao adicionad
+ *      409:
+ *       description: Produto ja existe
+ *      500:
+ *       description: Server Error
+ * 
+*/
 produtoRouter.route('/produtos')
   .post(prodPipAdd);
 
