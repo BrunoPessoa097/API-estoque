420500e4eeec509c2cb88ff5a3d71670abff51ae
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-24 08:22:28 +0000

    Atualizando o produtoJoi.
    - deixando os campos opcional para reaproveitamento.

--- src/schemas/joi/produtoJoi.ts
+++ src/schemas/joi/produtoJoi.ts
@@ -4,29 +4,29 @@ import produtoInput from '../../interfaces/produtoInterface';
 
 // produto joi
 const produtoJoi: ObjectSchema<produtoInput> = Joi.object({
-  nome: Joi.string().min(3).max(50).required().messages({
+  nome: Joi.string().min(3).max(50).messages({
     "string.empty": "Nome do produto nao pode estar em branco",
     "string.min": "Nome do produto tem que ter no mínimo {#limit} caracteres",
     "string.max": "Nome do produto tem que ter no máximo {#limit} caracteres",
     "any.required": "Campo nome requerido"
   }),
-  quantidade: Joi.number().integer().positive().required().messages({
+  quantidade: Joi.number().integer().positive().messages({
     "number.base": "Quantidade tem que ser um número",
     "number.positive": "Quantidade não pode ser um número negativo",
     "any.required": "Quantidade é obrigatório"
   }),
-  preco: Joi.number().positive().required().messages({
+  preco: Joi.number().positive().messages({
     "number.base": "Preço deve ser um número",
     "number.positive": "Preço tem que ser um valor positivo",
     "any.required":"Campo preço não pode ser vazio"
   }),
-  id_marca: Joi.string().length(24).required().messages({
+  id_marca: Joi.string().length(24).messages({
     "string base": "ID marca tem que ser um texto",
     "string.empty": "ID da marca não pode estar vazio",
     "string.length": "O ID marca tem que ter exatamente {#limit} caracteres",
     "any.required": "O ID marca e obrigatório"
   }),
-  id_categoria: Joi.string().length(24).required().messages({
+  id_categoria: Joi.string().length(24).messages({
     "string.base": "O ID categoria tem que ser um texto",
     "string.empty": "O ID cateria não pode estar vazio",
     "string.length": "O ID categoria tem que ter exatamente {#limit} caracteres",
