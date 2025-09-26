5fecdbb9cb54f5b2c63c93de227ac7cd3c647975
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-26 03:03:33 +0000

    melhoria marcaJoi
    - removendo obrigatoriedade dos campos

--- src/schemas/joi/marcaJoi.ts
+++ src/schemas/joi/marcaJoi.ts
@@ -4,19 +4,19 @@ import marcaInput from '../../interfaces/marcaInterface';
 
 // Criando o validador de marca.
 const marcaJoi: ObjectSchema<marcaInput> = Joi.object({
-  nome: Joi.string().min(3).max(20).required().messages({
+  nome: Joi.string().min(3).max(20).messages({
     "string.empty": "Nome não pode estar vazio",
     "string.min": "Nome tem que ter no mínimo {#limit} caracteres",
     "string.max": "Nome só pode ter nk máximo {#limit} caracteres",
     "any.required": "Nome é obrigatório"
   }),
-  nomeSocial: Joi.string().min(3).max(20).required().messages({
+  nomeSocial: Joi.string().min(3).max(20).messages({
     "string.empty":"Nome social não pode estar vazio",
     "string.min":"Nome social tem que ter no minimo {#limit} caracteres",
     "string.max":"Nome social tem que ter no máximo {#limit} caracteres",
     "any.required": "Nome social é obrigatório"
   }),
-  cnpj: Joi.string().min(11).max(14).required().messages({
+  cnpj: Joi.string().min(11).max(14).messages({
     "string.empty": "CNPJ não pode estar vazio",
     "string.min": "CNPJ tem que ter no mínimo {#limit} de carácteres",
     "string.max": "CNPJ tem que ter no máximo {#limit} de caracteres",
