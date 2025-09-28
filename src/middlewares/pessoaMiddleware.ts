200c04e04decfde0b5d949dfdc1b65e3c8d52f41
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-09-28 03:45:43 +0000

    Validação das entradas de pessoa
    - validando as entradas de pessoa
    - comentários

--- src/middlewares/pessoaMiddleware.ts
+++ src/middlewares/pessoaMiddleware.ts
@@ -1,15 +1,39 @@
 import { Request, Response } from 'express';
+import { ObjectSchema } from 'joi';
 // imports locais
 import pessoaInput from '../interfaces/pessoaInterface';
+import pessoaJoi from '../schemas/joi/pessoaJoi';
 
 /**
  * @description Padronizar entradas de pessoas
  */
 const pessoaPadronizar = (req: Request, res: Response) => {
   try{
-    res.status(200).json({
-      message: 'pessoa'
-    })
+    // desistruturando 
+    const { nome, endereco, dt_nasc, nivel, senha }: pessoaInput = req.body;
+
+    // recriando so com os pametros enviados
+    req.body = {
+      ...(nome && {nome: nome.trim()}),
+      ...(endereco && {endereco: endereco.trim()}),
+      ...(dt_nasc && {dt_nasc}),
+      ...(nivel && {nivel: nivel.trim()}),
+      ...(senha && {senha: senha.trim()})
+    }
+
+    // validando as entradas
+    const { error , value } = pessoaJoi.validate(req.body,{abortEarly: false});
+
+    // saidas dos erros
+    if(error) {
+      res.status(404).json({
+        message: error.details.map(e=>e.message)
+      });
+    }
+    // senão houver erros
+    else{
+      req.body = value;
+    }
   }
   // server error
   catch(error){
