6fa5fa889463ad14a87fbcd1e4c80234215e8346
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-10-01 03:36:48 +0000

    comentario

--- src/services/nivelServices.ts
+++ src/services/nivelServices.ts
@@ -1,6 +1,13 @@
 //import nivelInput, { nivelDocument } from '../interface/nivelInterface';
 import nivelMongo from '../schemas/mongoose/nivelSchema';
 
+/** 
+ * @description Verificar se existe nome já registrado
+ * @async
+ * @function existSigla
+ * @returns {Promise<boolean> } Retorna uma Promise de verdadeiro ou falso.
+ * @author Bruno Pessoa
+ */
 const existSigla = async(sigla: string): Promise<boolean> => {
   const exist: boolean = !!(await nivelMongo.exists({sigla}));
   if(exist){ throw new Error('Sigla já existe')}
