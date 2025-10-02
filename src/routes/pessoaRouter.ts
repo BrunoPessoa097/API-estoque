ef02b89316b6f1338f48b5a047445125f5f6ed7a
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-10-02 06:17:38 +0000

    adicionando ROute add
    - adicionando rota pessoa ao swagger

--- src/routes/pessoaRouter.ts
+++ src/routes/pessoaRouter.ts
@@ -5,6 +5,49 @@ import pessoaPipAdd from "../pipelines/pessoaPipeline";
 // construir rota
 const pessoaRouter: Router = Router();
 
+/**
+ * @swagger
+ * tags: Pessoas
+ * summary: Rotas de pessoa
+ */
+
+/**
+ * @swagger
+ * /pessoa:
+ *  post:
+ *    summary:  Rota para adicionar pessoa
+ *    description: Criar Pessoa
+ *    tags: [Pessoas]
+ *    requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *          type: object
+ *          properties:
+ *           nome:
+ *            type: string
+ *           email:
+ *            type: string
+ *           senha:
+ *            type: string
+ *           endereco:
+ *            type: string
+ *           dt_nasc:
+ *            type: string
+ *           nivel:
+ *            type: string
+ *    responses:
+ *      201:
+ *       description: Adicionado
+ *      404:
+ *       description: Erro ao adicionar
+ *      409:
+ *       description: nome e/ou email ja existe
+ *      500:
+ *       description: error variados
+ */
+
 pessoaRouter.route("/pessoa")
   .post(pessoaPipAdd);
 
