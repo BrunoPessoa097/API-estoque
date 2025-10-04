c7b8d2383444eddd0affc6ca1d534afd173c5b4f
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-10-04 04:57:36 +0000

    criptografia da senha

--- src/middlewares/_configMiddlewares.ts
+++ src/middlewares/_configMiddlewares.ts
@@ -1,5 +1,8 @@
+import bcrypt from 'bcrypt';
+
 /**
  * @description Funçao para deixar em maiúscula à primeira letra da palavra.
+ * @function palavraMaiuscula
  * @param {string} palavra - Palavra para ter maiúscula.
  * @returns string
  * @author Bruno Pessoa
@@ -10,6 +13,7 @@ const palavraMaiuscula = (palavra:string):string => {
 
 /**
  * @description Função para deixar tudo maiúsculo.
+ * @function palavraUpper
  * @param {string} - palavra - palavra para ser convertida
  * @return string
  * @author Bruno Pessoa
@@ -19,6 +23,7 @@ export const palavraUpper = (palavra: string):string => {
 };
 /**
  * @descriprion Converter para duas casas decimal
+ * @function numDecimal
  * @param {number} - numero - número para ter casa decimal
  * @return float
  * @author Bruno Pessoa
@@ -27,4 +32,21 @@ export const numDecimal = (numero:number):number => {
   return Number(numero.toFixed(2));
 }
 
+/**
+ * @descriprion fazer um hash para senha
+ * @function hashSenha
+ * @param {string} - senha - número para ser criptografado
+ * @return string
+ * @author Bruno Pessoa
+ */
+export const hashSenha = async(senha: string): Promise<string> => {
+  // recebendo os valores padrão
+  const vz: number = parseInt(`${process.env.HASH || 10}`);
+  // encriptando
+  const hash: string = await bcrypt.hash(senha, vz);
+  // retorno criptografado
+  return hash;
+}
+
+// export 
 export default palavraMaiuscula;
