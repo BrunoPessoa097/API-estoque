532cccd53a63cfd7129fa33fd63d1aac3ae308cd
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-10-11 05:44:34 +0000

    Adicionando RBAC
    - protegendo rotas por niveis

--- src/pipelines/produtoPipelines.ts
+++ src/pipelines/produtoPipelines.ts
@@ -1,9 +1,13 @@
 import { compose }from './_compose';
 import { produtoExistEntra, produtoVerificar, produtoPadronizar } from '../middlewares/produtoMiddlewares';
+import { loginAuthorValido, permissaoRota } from '../middlewares/authorMiddleware';
 import { produtoAdd, produtoList, produtoId, produtoUpdate, produtoDelete } from '../controllers/produtoControllers';
+import {nivel2,nivel3} from '../enum/niveis';
 
 // adicionanr produto.
 export const prodPipAdd = compose(
+  loginAuthorValido,
+  permissaoRota(...nivel2),
   produtoExistEntra,
   produtoVerificar,
   produtoPadronizar,
@@ -12,16 +16,22 @@ export const prodPipAdd = compose(
 
 // listar produto
 export const prodPipList = compose(
+  loginAuthorValido,
+  permissaoRota(...nivel3),
   produtoList
 );
 
 // selecionar produto
 export const prodPipId = compose(
+  loginAuthorValido,
+  permissaoRota(...nivel3),
   produtoId
 );
 
 // atualizar os campos de produto
 export const prodPipUpdate = compose(
+  loginAuthorValido,
+  permissaoRota(...nivel2),
   produtoExistEntra,
   produtoVerificar,
   produtoPadronizar,
@@ -30,5 +40,7 @@ export const prodPipUpdate = compose(
 
 // excluir produto
 export const prodPipDelete = compose(
+  loginAuthorValido,
+  permissaoRota(...nivel2),
   produtoDelete
 );
