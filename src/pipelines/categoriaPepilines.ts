b9d09f7eda2783b0d8fe12bcc405681d7b2e9c08
Bruno Pessoa <37255851+BrunoPessoa097@users.noreply.github.com>
2025-10-11 05:35:40 +0000

    Adicionando RBAC a categoria
    - adicionando proteção de niveis a categoria

--- src/pipelines/categoriaPepilines.ts
+++ src/pipelines/categoriaPepilines.ts
@@ -1,9 +1,13 @@
 import { compose } from './_compose';
 import { categoriaExist, categoriaValidar, categoriaPadronizar } from '../middlewares/categoriaMiddleware';
+import {loginAuthorValido, permissaoRota } from '../middlewares/authorMiddleware';
 import { categoriaAdd, categoriaAll, categoriaUnico, categoriaUpdate, categoriaDelete } from '../controllers/categoriaControllers';
+import { nivel2, nivel3 } from '../enum/niveis';
 
 // adicionar categoria 
 export const catPipAdd = compose(
+  loginAuthorValido, 
+  permissaoRota(...nivel2),
   categoriaExist,
   categoriaValidar,
   categoriaPadronizar,
@@ -12,16 +16,22 @@ export const catPipAdd = compose(
 
 // listar categoria 
 export const catPipList = compose(
+  loginAuthorValido, 
+  permissaoRota(...nivel3),
   categoriaAll
 );
 
 // mostrar categoria selecionada
 export const catPipId = compose(
+  loginAuthorValido, 
+  permissaoRota(...nivel2),
   categoriaUnico
 );
 
 // atualizar categoria 
 export const catPipUpdate = compose(
+  loginAuthorValido, 
+  permissaoRota(...nivel2),
   categoriaExist,
   categoriaValidar,
   categoriaPadronizar,
@@ -30,5 +40,7 @@ export const catPipUpdate = compose(
 
 // deletar categoria 
 export const catPipDelete = compose(
+  loginAuthorValido, 
+  permissaoRota(...nivel2),
   categoriaDelete
 );
